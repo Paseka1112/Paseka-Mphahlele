3e52483440723a735ed0db9b7a7492a12bacc862
Maramanyane <pasekammphahlele@gmail.com>
2026-07-15 01:25:02 +0200

    Add files via upload

---
+++ ispangram.py
@@ -0,0 +1,35 @@
+import sys
+import stdio
+from instream import InStream
+import string
+import csv
+
+def ispangram(s):
+    alphabet = string.ascii_lowercase
+    s = s.lower()
+    collected = ''
+
+    for char in s:
+        if char in alphabet and char not in collected:
+            collected = collected + char
+    
+    collected = sorted(collected)
+
+    if collected == list(alphabet):  return True
+    else:
+        return False
+        
+def main():
+    filename = sys.argv[1]
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            s = row[0]
+            result = ispangram(s)
+            stdio.writeln(result)
+
+if __name__ == '__main__': main()
+    
+
+
+
