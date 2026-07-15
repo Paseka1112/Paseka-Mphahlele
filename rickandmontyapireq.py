515c6e3e40442ee9ee424dd2475ddb2b88420e1e
Maramanyane <pasekammphahlele@gmail.com>
2026-07-15 13:19:35 +0200

    Add files via upload

---
+++ rickandmontyapireq.py
@@ -0,0 +1,34 @@
+#This was a guided tut
+import requests
+import pandas as pd
+
+url = "https://rickandmortyapi.com/api/"
+endpoint = 'character'
+
+def main_request(url, endpoint, x):
+
+    response = requests.get(url + endpoint + f'?page={x}')
+    return response.json()
+
+def get_pages(response):
+    return response['info']['pages']
+
+def parse_json(response):
+    charlist = []
+    for item in response['results']:
+        char = { 
+            'name' : item['name'],
+            'no_ep' : len(item['episode']),
+        }
+        
+        charlist.append(char)
+    return charlist
+
+mainlist = []
+data = main_request(url, endpoint, 1)
+for x in range(1, get_pages(data) + 1):
+    print(x)
+    mainlist.extend(parse_json(main_request(url, endpoint,x)))
+df = pd.DataFrame(mainlist)
+print(df.head())
+
