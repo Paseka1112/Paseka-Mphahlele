1fd560613b3933d124c2eb7f88eb77981af3acc7
Maramanyane <pasekammphahlele@gmail.com>
2026-07-17 21:56:26 +0200

    Add files via upload

---
+++ minimimum_scalar_product.py
@@ -0,0 +1,19 @@
+import sys
+import stdio
+
+def min_scalar(u, v):
+    """This function returns the minimum scalar product of two vecors u and v"""
+    x = sorted(u)
+    y = sorted(v, reverse= True)
+
+    result = 0
+    for i in range(len(x)):
+        result += x[i]*y[i]
+    return abs(result)
+
+def main():
+    u = [1, 3, -5]
+    v = [-2, 4, 1]
+    result = min_scalar(u, v)
+    stdio.writeln(result)
+if __name__ == '__main__': main()
